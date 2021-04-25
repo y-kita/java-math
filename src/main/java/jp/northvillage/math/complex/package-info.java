987842482fc27714b943a810f3fe320a0ee40f47
KITAMURA Yuichiro <yuichiro.kitamura@gmail.com>
2021-04-25 12:03:14 +0900

    create complex package

---
+++ src/main/java/jp/northvillage/math/complex/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * package-info.java
+ *
+ * Copyright (c) 2021 KITAMURA Yuichiro
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * Complex numbers.
+ *
+ * @since 1.0
+ */
+package jp.northvillage.math.complex;
