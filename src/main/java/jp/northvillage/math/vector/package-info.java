16bd1a1d6fb363ca0f8d8c27faed4510de2b15c2
KITAMURA Yuichiro <yuichiro.kitamura@gmail.com>
2021-04-29 16:55:26 +0900

    create vector package

---
+++ src/main/java/jp/northvillage/math/vector/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * package-info.java
+ *
+ * Copyright (c) 2021 KITAMURA Yuichiro
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+package jp.northvillage.math.vector;
